a1c93d1e64283f16be5ec67b49525268ec739e9a
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-12 23:09:15 +0600

    ninth commit

--- src/Components/EachQuiz/EachQuiz.js
+++ src/Components/EachQuiz/EachQuiz.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const EachQuiz = ({ option }) => {
+
+
+
+const EachQuiz = ({ option , correctAnswer }) => {
+  const handleClickRightAnswer = (correctAnswer) => {
+    toast.warning(correctAnswer, {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+
   return (
     <div>
       <div>
-        <p className="border-2 list	border-sky-600	border-solid mr-5 p-4 text-lg hover:cursor-pointer rounded-lg">
+        <p onClick={() => handleClickRightAnswer(correctAnswer)} className="border-2 list	border-sky-600	border-solid mr-5 p-4 text-lg hover:cursor-pointer rounded-lg">
           {option}
         </p>
+        <ToastContainer/>
       </div>
     </div>
   );
