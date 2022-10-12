10fc66812d6884a03b44a142b922f86b1f80b019
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-12 22:23:12 +0600

    eighth commit

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,27 +1,54 @@
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import EachQuiz from '../EachQuiz/EachQuiz';
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import EachQuiz from "../EachQuiz/EachQuiz";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Questions = ({eachquiz}) => {
-    const {question , options} = eachquiz;
-    return (
-        <div className='p-10 border-2	border-gray-500	border-solid mx-10 mb-5'>
-            <h1 className='text-lg font-semibold'>{question} <small> [See the Answer <FontAwesomeIcon icon={faEye}></FontAwesomeIcon> ]</small> </h1>
-            <div className='grid md:grid-cols-2 m-5 p-15 gap-4'>
-                {
-                    options.map(option => <EachQuiz
-                        key={option.id}
-                        option={option}
-                        ></EachQuiz>)
-                }
-            </div>
-        </div>
-    );
+const Questions = ({ eachquiz }) => {
+  const { question, options, correctAnswer } = eachquiz;
+  const handleViewRightAnswer = (correctAnswer) => {
+    toast.error(correctAnswer, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+  return (
+    <div className="p-10 border-2	border-gray-500	border-solid mx-10 mb-5">
+      <h1 className="text-lg font-semibold">
+        {question}{" "}
+        <small>
+          {" "}
+          [See the Correct Answer{" "}
+          <FontAwesomeIcon
+            onClick={() => handleViewRightAnswer(correctAnswer)}
+            icon={faEye}
+          ></FontAwesomeIcon>{" "}
+          ]
+          <ToastContainer />
+        </small>{" "}
+      </h1>
+      <div className="grid md:grid-cols-2 m-5 p-15 gap-4">
+        {options.map((option) => (
+          <EachQuiz
+            key={option.id}
+            option={option}
+            correctAnswer={correctAnswer}
+          ></EachQuiz>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Questions;
-
 
 /* 
 
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css';
 
 const Statistics = () => {
-    return (
-        <div>
-            <h1>This Is Statistics.</h1>
-        </div>
-    );
+  const data = [
+    {
+      id: 1,
+      name: "React",
+      logo: "https://live.staticflickr.com/65535/52413593240_e00326e727_o.png",
+      total: 8,
+    },
+    {
+      id: 2,
+      name: "JavaScript",
+      logo: "https://live.staticflickr.com/65535/52412638962_12e932256a_o.png",
+      total: 9,
+    },
+    {
+      id: 4,
+      name: "CSS",
+      logo: "https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png",
+      total: 8,
+    },
+    {
+      id: 5,
+      name: "Git",
+      logo: "https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png",
+      total: 11,
+    },
+  ];
+  return (
+    <div className="statistics">
+      <h1>Statistics Of Total Questions</h1>
+      <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+    </div>
+  );
 };
 
 export default Statistics;
